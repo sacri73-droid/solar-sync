95f19bb1fdd2c272e774b0f228444548fc9eb800
sacri73-droid <sacri73@gmail.com>
2026-03-30 09:24:13 +0200

    Update zcs_secrets.php

--- private/zcs_secrets.php
+++ private/zcs_secrets.php
@@ -1,31 +1,3 @@
-<?php
-return [
-    'cron_key' => 'questalachiavecronperzcs',
-
-    'db_host' => 'sql100.infinityfree.com',
-    'db_name' => 'if0_39750578_sacripantistefano',
-    'db_user' => 'if0_39750578',
-    'db_pass' => '2HzfmkCxTFoVy',
-    'db_charset' => 'utf8mb4',
-
-    'firebase_api_key' => 'AIzaSyCTFwAoDwQaUWFhMEHeqUDw-rvu65vuX_0',
-    'zcs_email' => 'sacri73@gmail.com',
-    'zcs_password' => 'Magicaroma_1',
-    'graphql_url' => 'https://yyhpek4qzrg25gh56dg4w3pnou.appsync-api.eu-west-1.amazonaws.com/graphql',
-
-    // SE NON LO METTI, il sync usa automaticamente solarman_device_sn
-    'zcs_serials' => [
-        'ZH1025006KE241210172',
-    ],
-
-    'solarman_site_id' => '63203749',
-    'solarman_device_id' => '238269913',
-    'solarman_device_sn' => 'ZH1025006KE241210172',
-
-    'solarman_cookie' => 'language=it; affixPath=/business/maintain/plant; accountFirstUse=eMail; guide_nav_apply=1; guide_nav_manage=1; guide_head_controls=1; guide_navDetail=1; igen_cookie_consent={"timestamp":"2026-01-29T11:20:05.786Z","categories":{"necessary":true,"analytics":true,"function":true},"version":"1.0","language":"it","cookieKeyList":[],"localKeyList":[]}; firstPrivacy=true; tokenKey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxMDc5MDY4OF9zYWNyaTczQGxpYmVyby5pdF8yIiwibW9kaWZ5X3Bhc3N3b3JkIjowLCJzY29wZSI6WyJhbGwiXSwiZGV0YWlsIjp7Im9yZ2FuaXphdGlvbklkIjoxMDc5MDY4OCwidG9wR3JvdXBJZCI6MTA0NzE4NzYsImdyb3VwSWQiOjEwNDcxODc2LCJyb2xlSWQiOjEsInVzZXJJZCI6MTU3MjM0ODIsInZlcnNpb24iOjEwMDEsImlkZW50aWZpZXIiOiJzYWNyaTczQGxpYmVyby5pdCIsImlkZW50aXR5VHlwZSI6MiwibWRjIjoiRk9SRUlHTl8xIiwiYXBwSWQiOm51bGwsIm5hbWUiOm51bGwsInJvbGVJZHMiOm51bGwsInRlbmFudE9yZ0lkIjpudWxsLCJzaWduUmVsSWQiOm51bGwsInRlbmFudExvZ2luRW51bSI6Ik9USEVSIn0sImV4cCI6MTc3OTY5NzI3NCwibWRjIjoiRk9SRUlHTl8xIiwiYXV0aG9yaXRpZXMiOlsiYWxsIl0sImp0aSI6IjFhNWE2MTlhLTk3ZTgtNDlmMy1hNWY1LTNkMTEyZWJkYTY4MiIsImNsaWVudF9pZCI6InRlc3QifQ.AcQ2kGpLQ2VMogVOs412VKF-3uZ-AX23vOdo56VHGiBWlrTNXuROpPcVk0Z3FIt-3VzhG8JEjRQp-S2UvZpzWL761zkCaCKkvKTa1sRxXPOYk4WS3oyFn8Z2-ZwBhFlznzFa4yjNmoDUO7UmptZRm5sA7t8SYfIZz0x03GwLu0NwldLb6OVBQSKP7rG8OUdbRD4Cc-I58LOtipWsQJfjJ2UCIAzOU2RLjg6MTBG089JsrPcLrexb1mY2PWYp62lYBdFen-cxbKrfzfwhxyJrrUvBKlpEq3R_os_-jLDw0jkDnagNzJ57Q4YBUU5zVIhBqxm1SkvO1Z3A-xJWE-21vg; acw_tc=0a0ca96017745256977101514e4e3b3c49292f771de188ada7a99701faa5ce',
-    'solarman_bearer_token' => 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxMDc5MDY4OF9zYWNyaTczQGxpYmVyby5pdF8yIiwibW9kaWZ5X3Bhc3N3b3JkIjowLCJzY29wZSI6WyJhbGwiXSwiZGV0YWlsIjp7Im9yZ2FuaXphdGlvbklkIjoxMDc5MDY4OCwidG9wR3JvdXBJZCI6MTA0NzE4NzYsImdyb3VwSWQiOjEwNDcxODc2LCJyb2xlSWQiOjEsInVzZXJJZCI6MTU3MjM0ODIsInZlcnNpb24iOjEwMDEsImlkZW50aWZpZXIiOiJzYWNyaTczQGxpYmVyby5pdCIsImlkZW50aXR5VHlwZSI6MiwibWRjIjoiRk9SRUlHTl8xIiwiYXBwSWQiOm51bGwsIm5hbWUiOm51bGwsInJvbGVJZHMiOm51bGwsInRlbmFudE9yZ0lkIjpudWxsLCJzaWduUmVsSWQiOm51bGwsInRlbmFudExvZ2luRW51bSI6Ik9USEVSIn0sImV4cCI6MTc3OTY5NzI3NCwibWRjIjoiRk9SRUlHTl8xIiwiYXV0aG9yaXRpZXMiOlsiYWxsIl0sImp0aSI6IjFhNWE2MTlhLTk3ZTgtNDlmMy1hNWY1LTNkMTEyZWJkYTY4MiIsImNsaWVudF9pZCI6InRlc3QifQ.AcQ2kGpLQ2VMogVOs412VKF-3uZ-AX23vOdo56VHGiBWlrTNXuROpPcVk0Z3FIt-3VzhG8JEjRQp-S2UvZpzWL761zkCaCKkvKTa1sRxXPOYk4WS3oyFn8Z2-ZwBhFlznzFa4yjNmoDUO7UmptZRm5sA7t8SYfIZz0x03GwLu0NwldLb6OVBQSKP7rG8OUdbRD4Cc-I58LOtipWsQJfjJ2UCIAzOU2RLjg6MTBG089JsrPcLrexb1mY2PWYp62lYBdFen-cxbKrfzfwhxyJrrUvBKlpEq3R_os_-jLDw0jkDnagNzJ57Q4YBUU5zVIhBqxm1SkvO1Z3A-xJWE-21vg',
-];
-?>
 
 
 
